fd9ed5ef7738dab98e26516e8d332bff5ad9bcc7
Prachi-Saini <115067278+Prachi-Saini@users.noreply.github.com>
2023-07-12 16:54:31 +0530

    Time: 129 ms (93.80%), Space: 57.2 MB (93.91%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -4,7 +4,8 @@ public:
         sort(nums.begin(),nums.end());
         bool flag = false;
         for(int i =0;i<nums.size()-1;i++){
-            if(nums[i] == nums[i+1]) return true;
+            if(nums[i] == nums[i+1]) 
+                return true;
         }
         return flag;
     }
